56f7d3b2f8d3a850277481d59f0fdad101edc297
poolshark1904 <112887393+poolshark1904@users.noreply.github.com>
2023-12-02 01:41:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 document.getElementById("santaForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var form = event.target;
-    var formData = new FormData(form);
+  var form = event.target;
+  var formData = new FormData(form);
 
-    fetch(form.action, {
-        method: form.method,
-        body: formData
-    })
-    .then(response => response.json())  // Parse the JSON response
-    .then(data => {
-        displayMessage(data);
-    })
-    .catch(error => console.error('Error:', error));
+  fetch(form.action, {
+    method: form.method,
+    body: formData
+  })
+  .then(response => response.json()) // Parse the JSON response
+  .then(data => {
+    displayMessage(data);
+  })
+  .catch(error => console.error('Error:', error));
 });
 
 function displayMessage(data) {
-    var messageContainer = document.getElementById("message");
-    
-    if (data.status === "success") {
-        // If the status is "success", hide the form
-        document.getElementById("santaForm").style.display = "none";
-    } else {
-        // If there is an error, clear the email field and focus on it
-        document.getElementById("email").value = "";
-        document.getElementById("email").focus();
-    }
+  var messageContainer = document.getElementById("message");
 
-    // Display the message from the server
-    messageContainer.innerHTML = '<p>' + data.message + '</p>';
-    messageContainer.style.display = "block";
+  if (data.status === "success") {
+    // If the status is "success", hide the form
+    document.getElementById("santaForm").style.display = "none";
+  } else {
+    // If there is an error, clear the email field and focus on it
+    document.getElementById("email").value = "";
+    document.getElementById("email").focus();
+  }
+
+  // Display the message from the server
+  messageContainer.innerHTML = '<p>' + data.message + '</p>';
+  messageContainer.style.display = "block";
 }
-
-
-
